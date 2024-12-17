f5bc7e27bd7018bf7264a2e09d8df40fcf9afca6
Z Yin <Z.Yin.MD@gmail.com>
2024-12-17 13:30:20 -0500

    specify ports forts for local dev servers

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-})
+  server: { port: 5525, strictPort: true, open: true },
+  preview: { port: 5510, strictPort: true, open: true },
+});
