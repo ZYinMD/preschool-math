96393d51020d68360f2901574a5034a89b1b50d1
Z Yin <Z.Yin.MD@gmail.com>
2025-02-07 15:38:44 -0500

    change components in libs to be props based

--- src/Game/Game.svelte
+++ src/Game/Game.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
   import QuestionBar from "../lib/QuestionBar/QuestionBar.svelte";
   import { states } from "../states/states.svelte";
+  const currentQuestion = states.currentQuestion;
 </script>
 
 <!-- @component the game view -->
 <div class="component">
-  <QuestionBar />
+  {#if currentQuestion}
+    <QuestionBar a={currentQuestion[0]} b={currentQuestion[1]} />
+  {:else}
+    <div>No question??</div>
+  {/if}
+
   <button on:click={() => (states.view = "settings")}>New</button>
 </div>
 
--- src/lib/QuestionBar/QuestionBar.svelte
+++ src/lib/QuestionBar/QuestionBar.svelte
@@ -1,21 +1,15 @@
 <script lang="ts">
-  import { states } from "../../states/states.svelte";
   import Tiles1to10 from "./Tiles1to10.svelte";
   import Tiles1to20 from "./Tiles1to20.svelte";
-  const currentQuestion = states.currentQuestion;
-  const total = currentQuestion ? currentQuestion[0] + currentQuestion[1] : 0;
+  const { a, b } = $props();
 </script>
 
 <!-- @component the row of tiles -->
 <div class="container">
-  {#if currentQuestion}
-    {#if total <= 10}
-      <Tiles1to10 a={currentQuestion[0]} b={currentQuestion[1]} />
-    {:else}
-      <Tiles1to20 a={currentQuestion[0]} b={currentQuestion[1]} />
-    {/if}
+  {#if a + b <= 10}
+    <Tiles1to10 {a} {b} />
   {:else}
-    <div>No question??</div>
+    <Tiles1to20 {a} {b} />
   {/if}
 </div>
 
