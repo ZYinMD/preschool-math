748e79d8fdde602679585415906209dc0eadac92
Z Yin <Z.Yin.MD@gmail.com>
2025-02-08 16:24:01 -0500

    refactor: no longer use props with tiles bar, connect to states directly

--- src/GameBoard/GameBoard.svelte
+++ src/GameBoard/GameBoard.svelte
@@ -1,15 +1,14 @@
 <script lang="ts">
+  import { s } from "../states/states.svelte";
   import AnswerBar from "./AnswerBar/AnswerBar.svelte";
   import KeyPad from "./KayPad/KeyPad.svelte";
   import TilesBar from "./TilesBar/TilesBar.svelte";
-  import { s } from "../states/states.svelte";
-  const { a, b } = s.currentQuestion.value;
 </script>
 
 <!-- @component the game view -->
 <div class="component">
   <button on:click={() => (s.view = "settings")}>New</button>
-  <TilesBar {a} {b} />
+  <TilesBar />
   <AnswerBar />
   <KeyPad />
 </div>
--- src/GameBoard/TilesBar/TilesBar.svelte
+++ src/GameBoard/TilesBar/TilesBar.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
+  import { s } from "../../states/states.svelte";
   import OneTile from "./OneTile.svelte";
   const array1to10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   const array1to20 = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
-  const { a, b } = $props();
+  const { a, b } = s.currentQuestion.value;
   const allNumbers = a + b > 10 ? array1to20 : array1to10;
 </script>
 
