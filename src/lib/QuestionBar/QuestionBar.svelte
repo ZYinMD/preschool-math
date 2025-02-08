c992a2480c4debd979f44921a6b7fa862ad460f9
Z Yin <Z.Yin.MD@gmail.com>
2025-02-07 19:07:40 -0500

    simplify components

--- src/lib/QuestionBar/QuestionBar.svelte
+++ src/lib/QuestionBar/QuestionBar.svelte
@@ -1,17 +1,22 @@
 <script lang="ts">
-  import Tiles1to10 from "./Tiles1to10.svelte";
-  import Tiles1to20 from "./Tiles1to20.svelte";
+  import OneTile from "./OneTile.svelte";
+  const array1to10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const array1to20 = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
   const { a, b } = $props();
+  const allNumbers = a + b > 10 ? array1to20 : array1to10;
 </script>
 
 <!-- @component the row of tiles -->
-<div class="container">
-  {#if a + b <= 10}
-    <Tiles1to10 {a} {b} />
-  {:else}
-    <Tiles1to20 {a} {b} />
-  {/if}
+<div class="component">
+  {#each allNumbers as i (i)}
+    <OneTile {a} {b} nth={i} />
+  {/each}
 </div>
 
 <style>
+  .component {
+    display: flex;
+  }
 </style>
