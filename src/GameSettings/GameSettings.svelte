71beebc70d66d88f4d43fe4d755877fdda243a9a
Z Yin <Z.Yin.MD@gmail.com>
2025-02-10 10:55:38 -0500

    refactor: extract the "restart game" function

--- src/GameSettings/GameSettings.svelte
+++ src/GameSettings/GameSettings.svelte
@@ -1,27 +1,6 @@
 <script lang="ts">
-  import { d, s } from "../states/states.svelte";
+  import { d, restartGame, s } from "../states/states.svelte";
   let { allowQuestionStartingWith } = s.settings;
-
-  /**
-   * Stackoverflow answer of how to shuffle an array.
-   * This function first makes a copy of the array, then mutates the copy, then returns it.
-   */
-  function shuffle(array: any[]) {
-    const result = [...array];
-    let currentIndex = result.length;
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-      // Pick a remaining element...
-      let randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-      // And swap it with the current element.
-      [result[currentIndex], result[randomIndex]] = [
-        result[randomIndex],
-        result[currentIndex],
-      ];
-    }
-    return result;
-  }
 </script>
 
 <!-- @component the settings view -->
@@ -74,13 +53,8 @@
   <div>
     <button
       onclick={() => {
+        restartGame();
         s.view = "game";
-        // on click start, randomly pick x questions from derived.allowedQuestions, where x is states.settings.numQuestions, and put them in states.questionsThisGame
-        s.questionsThisGame = shuffle(d.questionPool).slice(
-          0,
-          s.settings.numQuestions
-        );
-        s.nowAt = 0;
       }}>Start</button
     >
   </div>
--- src/states/states.svelte.ts
+++ src/states/states.svelte.ts
@@ -137,3 +137,33 @@ export const d = {
     return currentQuestion;
   },
 };
+
+export function restartGame() {
+  /**
+   * Stackoverflow answer of how to shuffle an array.
+   * This function first makes a copy of the array, then mutates the copy, then returns it.
+   */
+  function shuffle(array: any[]) {
+    const result = [...array];
+    let currentIndex = result.length;
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+      // Pick a remaining element...
+      let randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+      // And swap it with the current element.
+      [result[currentIndex], result[randomIndex]] = [
+        result[randomIndex],
+        result[currentIndex],
+      ];
+    }
+    return result;
+  }
+
+  // randomly pick x questions from derived.allowedQuestions, where x is states.settings.numQuestions, and put them in states.questionsThisGame
+  s.questionsThisGame = shuffle(d.questionPool).slice(
+    0,
+    s.settings.numQuestions
+  );
+  s.nowAt = 0;
+}
