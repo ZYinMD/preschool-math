6bdcce6a257b4d5997995b0fc5a795f4ec0912fb
Z Yin <Z.Yin.MD@gmail.com>
2025-02-21 13:48:58 -0500

    auto start game on reload

--- src/GameBoard/GameBoard.svelte
+++ src/GameBoard/GameBoard.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
+  import { restartGame, s } from "../states/states.svelte";
   import AnswerBar from "./AnswerBar/AnswerBar.svelte";
   import Header from "./Header/Header.svelte";
   import KeyPad from "./KayPad/KeyPad.svelte";
   import TilesBar from "./TilesBar/TilesBar.svelte";
+  $effect(() => {
+    // unlike effect in react, with $effect in svelte5 you can't specify what variables to monitor for rerun. The effect will always rerun if anything referenced in the function changes, and it could lead to infinite loops. A trick is that anything referenced inside async code won't be monitored. So, we can put everything in async if we're sure we only want to run once.
+    setTimeout(() => {
+      const gameHasStarted = s.questionsThisGame.length > 1;
+      if (!gameHasStarted) restartGame();
+    }, 0);
+  });
 </script>
 
 <!-- @component the game view -->
