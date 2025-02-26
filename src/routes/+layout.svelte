57f508dc4de8455225c38550ba1a33c169e29ca4
Z Yin <Z.Yin.MD@gmail.com>
2025-02-25 20:46:15 -0500

    change page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import "../app.css";
+  let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>Preschool Math</title>
+</svelte:head>
 {@render children()}
